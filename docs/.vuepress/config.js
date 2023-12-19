815702550af52b76a3c98e86ce60532f9582e66a
刘玉婷 <liuyuting7@xiaomi.com>
2023-12-19 23:44:03 +0800

    feat: 添加顶部导航和侧边栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  title: "婷的笔记",
+  themeConfig: {
+    logo: "/assets/img/logo.png",
+    repo: "https://github.com/lyt2045817743/CloudNote",
+    sidebar: {
+      '/frontend/': [
+        {
+          title: '前端基础',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+          children: [
+            '/frontend/base/html_css/',
+            '/frontend/base/js/'
+          ]
+        },
+        {
+          title: '前端进阶',
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+          children: [
+            '/frontend/premium/browser/',
+            '/frontend/premium/code/'
+          ],
+        }
+      ],
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "前端体系",
+        items: [
+          {
+            text: "基础知识",
+            items: [
+              { text: "HTML&CSS", link: "/frontend/base/html_css/" },
+              { text: "JS", link: "/frontend/base/js/" },
+            ],
+          },
+          {
+            text: "进阶",
+            items: [
+              { text: "浏览器", link: "/frontend/premium/browser/" },
+              { text: "代码优化", link: "/frontend/premium/code/" },
+              { text: "工程化", link: "/frontend/premium/engineering/" },
+            ],
+          },
+        ],
+      },
+      {
+        text: "成长",
+        items: [{ text: "学习方法", link: "/growth/study/" }],
+      },
+    ],
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,8 @@
-# Hello VuePress
+---
+home: true
+heroImage: /assets/img/logo.png
+heroText: 婷的笔记
+tagline: 记录各种笔记：前端知识学习、读书笔记等
+actionText: 立刻进入 →
+actionLink: /frontend/base/html_css/
+---
